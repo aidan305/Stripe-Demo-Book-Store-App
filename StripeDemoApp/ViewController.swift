3f2af226131910abaf74ca56d5be5ef834387c43
aidan egan <eganjessie@hotmail.com>
2020-07-07 14:47:36 +0100

    Initial Commit

---
+++ StripeDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StripeDemoApp
+//
+//  Created by aidan egan on 07/07/2020.
+//  Copyright © 2020 aidan egan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
