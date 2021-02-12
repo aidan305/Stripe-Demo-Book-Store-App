326a6fc5972839d0433d640b425d50faf14d9a2b
aidan egan <eganjessie@hotmail.com>
2021-02-12 12:16:54 +0000

    fixing stripe

---
+++ StripeDemoApp/StripeDemoApp-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  StripeDemoApp-Bridging-Header.h
+//  StripeDemoApp
+//
+//  Created by aidan egan on 12/02/2021.
+//  Copyright © 2021 aidan egan. All rights reserved.
+//
+
+#ifndef StripeDemoApp_Bridging_Header_h
+#define StripeDemoApp_Bridging_Header_h
+
+
+#endif /* StripeDemoApp_Bridging_Header_h */
